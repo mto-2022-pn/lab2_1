8c13daa267a349bbc2abee0ad1030b948bff8296
Jakub Wlaźlak <kubikapb@gmail.com>
2022-03-16 10:42:04 +0100

    testy_v1 test noKeyInSequenceWithOneElement implementation

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -7,17 +7,24 @@ import org.junit.jupiter.api.Test;
 
 
 class BinarySearchTest {
+    int[] sequenceWithOneElement = new int[]{13};
 
     @BeforeEach
     void setUp() throws Exception {}
 
     @Test
     void keyIsInSequenceWithOneElement() {
-        int[] sequenceWithOneElement = new int[]{13};
         int key = 13;
         SearchResult searchResult = BinarySearch.search(key, sequenceWithOneElement);
         assertTrue(searchResult.isFound());
         assertEquals(key, sequenceWithOneElement[searchResult.getPosition()]);
+    }
+    @Test
+    void noKeyInSequenceWithOneElement() {
+        int key=12;
+        SearchResult searchResult = BinarySearch.search(key,sequenceWithOneElement);
+        assertFalse(searchResult.isFound());
+        assertEquals(-1,searchResult.getPosition());
     }
 
 }
