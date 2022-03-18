022ec1d0e0ad9710a3c1f7ed601b425ee5aee67f
Norbert Łodziński <209382@edu.p.lodz.pl>
2022-03-18 22:34:19 +0100

    testy_v1
    added tests for sequence length greater than 1, when:
    -key is first in sequence
    -key is last in sequence
    -key is in the middle of sequence
    -key is not in sequence

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -13,7 +13,7 @@ class BinarySearchTest {
 
     @Test
     void seqLengthIsOne_keyInSeq() {
-        int key = 1;
+        int key = 10;
         int[] seq = {key};
 
         SearchResult result = BinarySearch.search(key, seq);
@@ -25,10 +25,58 @@ class BinarySearchTest {
 
     @Test
     void seqLengthIsOne_keyNotInSeq() {
-        int key = 1;
+        int key = 10;
         int[] seq = {key};
 
         SearchResult result = BinarySearch.search(key + 1, seq);
+
+        assertFalse(result.isFound());
+        assertEquals(-1, result.getPosition());
+    }
+
+    @Test
+    void seqLengthGreaterThanOne_keyFirstInSeq() {
+        int key = 10;
+        int[] seq = {key, 20, 30};
+
+        SearchResult result = BinarySearch.search(key, seq);
+
+        assertTrue(result.isFound());
+        assertEquals(0, result.getPosition());
+        assertEquals(key, seq[result.getPosition()]);
+    }
+
+    @Test
+    void seqLengthGreaterThanOne_keyLastInSeq() {
+        int key = 30;
+        int[] seq = {10, 20, key};
+
+        SearchResult result = BinarySearch.search(key, seq);
+
+        assertTrue(result.isFound());
+        assertEquals(seq.length - 1, result.getPosition());
+        assertEquals(key, seq[result.getPosition()]);
+    }
+
+    @Test
+    void seqLengthGreaterThanOne_keyInMiddleOfSeq() {
+        int key = 20;
+        int[] seq = {10, key, 30};
+
+        SearchResult result = BinarySearch.search(key, seq);
+
+        assertTrue(result.isFound());
+        assertNotEquals(seq.length - 1, result.getPosition());
+        assertNotEquals(0, result.getPosition());
+        assertEquals(key, seq[result.getPosition()]);
+    }
+
+    @Test
+    void seqLengthGreaterThanOne_keyNotInSeq() {
+        int key = 20;
+        int[] seq = {10, 30, 40};
+
+        SearchResult result = BinarySearch.search(key, seq);
 
         assertFalse(result.isFound());
         assertEquals(-1, result.getPosition());
