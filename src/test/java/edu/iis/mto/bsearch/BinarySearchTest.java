36ed276b7f7f5393b78710943245f4947788f467
Hubert Krauza <hubertkrauza@gmail.com>
2022-03-19 02:00:51 +0100

    testy_v1 test notExistingElem_singleItemSequence

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -15,9 +15,18 @@ class BinarySearchTest {
     void searchForExistingElem_singleItemSequence() {
         int[] seq = new int[1];
         seq[0] = 4;
-        SearchResult searchResult = BinarySearch.search(4, seq);
+        SearchResult searchResult = BinarySearch.search(seq[0], seq);
         assertTrue(searchResult.isFound());
         assertEquals(1, searchResult.getPosition());
     }
 
+
+    @Test
+    void notExistingElem_singleItemSequence() {
+        int[] seq = new int[1];
+        seq[0] = 4;
+        SearchResult searchResult = BinarySearch.search(8, seq);
+        assertFalse(searchResult.isFound());
+        assertEquals(-1, searchResult.getPosition());
+    }
 }
