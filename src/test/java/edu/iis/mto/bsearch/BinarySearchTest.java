353a0ce9fd694122dcc60b06e90464d0d2b01880
Hubert Krauza <hubertkrauza@gmail.com>
2022-03-21 17:36:29 +0100

    testy_v1 fix tests and code for wrong returning position

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -17,7 +17,7 @@ class BinarySearchTest {
         seq[0] = 4;
         SearchResult searchResult = BinarySearch.search(seq[0], seq);
         assertTrue(searchResult.isFound());
-        assertEquals(1, searchResult.getPosition());
+        assertEquals(0, searchResult.getPosition());
     }
 
     @Test
@@ -34,7 +34,7 @@ class BinarySearchTest {
         int[] seq = new int[]{1,4,6,8,12,14,18,34,65};
         SearchResult searchResult = BinarySearch.search(seq[0], seq);
         assertTrue(searchResult.isFound());
-        assertEquals(1, searchResult.getPosition());
+        assertEquals(0, searchResult.getPosition());
     }
 
     @Test
@@ -42,6 +42,14 @@ class BinarySearchTest {
         int[] seq = new int[]{1,4,6,8,12,14,18,34,65};
         SearchResult searchResult = BinarySearch.search(seq[seq.length-1], seq);
         assertTrue(searchResult.isFound());
-        assertEquals(seq.length, searchResult.getPosition());
+        assertEquals(seq.length-1, searchResult.getPosition());
+    }
+
+    @Test
+    void searchForExistingElem_atMiddlePosition_multipleItemSequence() {
+        int[] seq = new int[]{1,4,6,8,12,14,18,34,65};
+        SearchResult searchResult = BinarySearch.search(seq[(seq.length)/2], seq);
+        assertTrue(searchResult.isFound());
+        assertEquals(seq.length/2, searchResult.getPosition());
     }
 }
