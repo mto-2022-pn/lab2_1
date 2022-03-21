328114e407e341eb18594d80444186f7c8ed435d
Piotr Pietrzak <228832@edu.p.lodz.pl>
2022-03-21 18:11:49 +0100

    testy_v2 update and refactor tests - Zadanie 4

--- src/main/java/edu/iis/mto/bsearch/BinarySearch.java
+++ src/main/java/edu/iis/mto/bsearch/BinarySearch.java
@@ -3,6 +3,9 @@
  */
 package edu.iis.mto.bsearch;
 
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 /**
  * Klasa implementująca wyszukiwanie binarne
  *
@@ -22,6 +25,10 @@ public class BinarySearch {
      *         sekwencji, jezeli nie znaleziony -1)
      */
     public static SearchResult search(int key, int[] seq) {
+        if (seq.length == 0)
+            throw new IllegalArgumentException();
+        if (!IntStream.range(0, seq.length - 1).allMatch(i -> seq[i] < seq[i + 1]))
+            throw new IllegalArgumentException();
         int start = 0;
         int end = seq.length - 1;
         int center;
--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -4,13 +4,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 
 class BinarySearchTest {
 
     @BeforeEach
     void setUp() throws Exception {}
-
+    @Test
+    void TestIfSequenceIsEmpty() {
+        int[] sequence = new int[0];
+        int key = 1;
+        assertThrows(IllegalArgumentException.class, () -> BinarySearch.search(key, sequence));
+    }
     @Test
     void TestOneElementInSequenceIsPresent() {
         int[] sequence = { 1 };
@@ -57,5 +63,17 @@ class BinarySearchTest {
         int key = -13;
         SearchResult result = BinarySearch.search(key, sequence);
         assertFalse(result.isFound() && result.getPosition() == -1);
+    }
+    @Test
+    void TestIfSequenceIsSorted() {
+        int[] sequence = { -1, 2, -3, 5, -8, 13 };
+        int key = 1;
+        assertThrows(IllegalArgumentException.class, () -> BinarySearch.search(key, sequence));
+    }
+    @Test
+    void TestIfSequenceIsContainsDuplicates() {
+        int[] sequence = { 2, 3, 3, 5, 7, 13 };
+        int key = 1;
+        assertThrows(IllegalArgumentException.class, () -> BinarySearch.search(key, sequence));
     }
 }
