29f2e41399f9748005ef9b5af910238ae8e3f9e0
TomaszOnopa <228818@edu.p.lodz.pl>
2022-03-21 17:47:54 +0100

    testy_v1 - Utworzenie testów

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -12,8 +12,56 @@ class BinarySearchTest {
     void setUp() throws Exception {}
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void keyIsInSeqSeqLenIsOne() {
+        int key = 4;
+        int[] seq = {4};
+        SearchResult result = BinarySearch.search(key, seq);
+        assertTrue(result.isFound());
+        assertEquals(result.getPosition(), 1);
     }
 
+    @Test
+    void keyIsNotInSeqSeqLenIsOne() {
+        int key = 7;
+        int[] seq = {4};
+        SearchResult result = BinarySearch.search(key, seq);
+        assertFalse(result.isFound());
+        assertEquals(result.getPosition(), -1);
+    }
+
+    @Test
+    void keyIsFirstInSeqSeqLenIsGreaterThanOne() {
+        int key = 3;
+        int[] seq = {3, 5, 45, 55, 78};
+        SearchResult result = BinarySearch.search(key, seq);
+        assertTrue(result.isFound());
+        assertEquals(result.getPosition(), 1);
+    }
+
+    @Test
+    void keyIsLastInSeqSeqLenIsGreaterThanOne() {
+        int key = 78;
+        int[] seq = {3, 5, 45, 55, 78};
+        SearchResult result = BinarySearch.search(key, seq);
+        assertTrue(result.isFound());
+        assertEquals(result.getPosition(), seq.length);
+    }
+
+    @Test
+    void keyIsInSeqSeqLenIsGreaterThanOne() {
+        int key = 45;
+        int[] seq = {3, 5, 45, 55, 78};
+        SearchResult result = BinarySearch.search(key, seq);
+        assertTrue(result.isFound());
+        assertEquals(result.getPosition(), 3);
+    }
+
+    @Test
+    void keyIsNotInSeqSeqLenIsGreaterThanOne() {
+        int key = 100;
+        int[] seq = {3, 5, 45, 55, 78};
+        SearchResult result = BinarySearch.search(key, seq);
+        assertFalse(result.isFound());
+        assertEquals(result.getPosition(), -1);
+    }
 }
