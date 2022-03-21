eadeb444789e66e18b62c7e90bbdfd9170ed9453
Wojciech Farej <228730@edu.p.lodz.pl>
2022-03-21 17:47:05 +0100

    testy_v1 first test, and first fix

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -5,15 +5,18 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 class BinarySearchTest {
 
     @BeforeEach
-    void setUp() throws Exception {}
-
-    @Test
-    void test() {
-        fail("Not yet implemented");
+    void setUp() throws Exception {
     }
 
+    @Test
+    void keyElementIsInSequence() {
+        int key = 5;
+        int[] seq = { 1,3,4,5,6,8 };
+        SearchResult searchResult = BinarySearch.search(key, seq);
+        assertTrue(searchResult.isFound());
+        assertEquals(seq[searchResult.getPosition()], key);
+    }
 }
