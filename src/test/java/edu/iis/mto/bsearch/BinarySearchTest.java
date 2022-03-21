ebd1de68600e567e20b1f647c62cf76d5121c3f3
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-03-21 18:04:01 +0100

    test_v1 added unit test.

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -12,8 +12,52 @@ class BinarySearchTest {
     void setUp() throws Exception {}
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void testSingleElemPresentInList() {
+        int[] seq = {1};
+        SearchResult result = BinarySearch.search(seq[0], seq);
+        assertTrue(result.isFound());
+        assertEquals(result.getPosition(), 0);
     }
 
+    @Test
+    void TestSingleElemNotPresent() {
+        int[] seq = {2};
+        SearchResult result = BinarySearch.search(1, seq);
+        assertFalse(result.isFound());
+        assertEquals(result.getPosition(), -1);
+    }
+
+    @Test
+    void TestMultiElemFirst() {
+        int[] seq = {2, 4, 5, 7};
+        SearchResult result = BinarySearch.search(seq[0], seq);
+        assertTrue(result.isFound());
+        assertEquals(result.getPosition(), 0);
+    }
+
+    @Test
+    void TestMultiElemLast() {
+        int[] seq = {2, 4, 5, 7};
+        var lastIdx = seq.length - 1;
+        SearchResult result = BinarySearch.search(seq[lastIdx], seq);
+        assertTrue(result.isFound());
+        assertEquals(result.getPosition(), lastIdx);
+    }
+
+    @Test
+    void TestMultiElemMiddle() {
+        int[] seq = {2, 3, 4, 6, 8};
+        var idx = (seq.length - 1) / 2;
+        SearchResult result = BinarySearch.search(seq[idx], seq);
+        assertTrue(result.isFound());
+        assertEquals(result.getPosition(), idx);
+    }
+
+    @Test
+    void TestMultiElemNotPresent() {
+        int[] seq = {2, 3, 4, 6};
+        SearchResult result = BinarySearch.search(12, seq);
+        assertFalse(result.isFound());
+        assertEquals(result.getPosition(), -1);
+    }
 }
