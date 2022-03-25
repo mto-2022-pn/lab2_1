e7cf8b35507174024e11fc6c0e5522c21895ed77
piterm11 <piteralke@gmail.com>
2022-03-25 21:02:41 +0100

    testy_v2 testy rzucające wyjątki oraz wykorzystanie dobrych praktyk

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -6,53 +6,77 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
+import java.util.Random;
+
 
 class BinarySearchTest {
 
     @BeforeEach
-    void setUp() throws Exception {
+    public void setUp() throws Exception {
     }
     @Test
-    void SequenceIsEmpty(){
+    public void SequenceIsEmpty(){
         int[] arr = new int[0];
-        assertThrows(IllegalArgumentException.class, ()->{
+        IllegalArgumentException e= assertThrows(IllegalArgumentException.class, ()->{
             BinarySearch.search(1, arr);
         });
+        assertEquals("Sequence length cannot be zero", e.getMessage());
     }
 
     @Test
-    void searchForValueInArray() {
+    public void SequenceIsNotSortedAsc(){
+        int[] arr = new int[]{4,2,1,5};
+        IllegalArgumentException e= assertThrows(IllegalArgumentException.class, ()->{
+            BinarySearch.search(1, arr);
+        });
+        assertEquals("Sequence must be sorted in ascending order", e.getMessage());
+    }
+
+    @Test
+    public void SequenceHasDupes(){
+        Random random = new Random();
+
+        int[] arr = new int[]{1,1,3,5};
+        IllegalArgumentException e= assertThrows(IllegalArgumentException.class, ()->{
+            BinarySearch.search(1, arr);
+        });
+        assertEquals("Sequence cannot have duplicates", e.getMessage());
+    }
+
+
+    @Test
+    public void searchForValueInArray() {
         SearchResult sr = BinarySearch.search(1, new int[]{1});
         assertTrue(sr.isFound());
     }
 
     @Test
-    void NotFoundInSequence() {
+    public void NotFoundInSequence() {
         SearchResult sr = BinarySearch.search(2, new int[]{1});
         assertTrue(!sr.isFound() && sr.getPosition() == -1);
     }
 
     @Test
-    void FirstElementInSequence() {
+    public void FirstElementInSequence() {
         SearchResult sr = BinarySearch.search(2, new int[]{2, 4, 7, 8});
         assertTrue(sr.isFound() && sr.getPosition() == 1);
     }
 
     @Test
-    void LastElementInSequence() {
+    public void LastElementInSequence() {
         int[] arr = new int[]{2, 4, 7, 8};
         SearchResult sr = BinarySearch.search(8, arr);
         assertTrue(sr.isFound() && sr.getPosition() == arr.length);
     }
 
     @Test
-    void MiddleElementInSequence() {
+    public void MiddleElementInSequence() {
         int[] arr = new int[]{2, 4, 5, 7, 8};
         SearchResult sr = BinarySearch.search(5, arr);
         assertTrue(sr.isFound() && sr.getPosition()>0 && sr.getPosition() < arr.length);
     }
     @Test
-    void NotFoundInLongSequence() {
+    public void NotFoundInLongSequence() {
         SearchResult sr = BinarySearch.search(2, new int[]{1,3,5,6,7,8,9});
         assertTrue(!sr.isFound() && sr.getPosition() == -1);
     }
