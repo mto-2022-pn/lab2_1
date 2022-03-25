e87c788e736707075bd74921c8ccceaa5ce29705
Adrian Pietrzak <78472697+AdrianPietrz@users.noreply.github.com>
2022-03-25 11:37:12 +0100

    testy_v1 dodanie testów zgodnych z przedstawioną tabelą

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -5,15 +5,69 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 
 class BinarySearchTest {
 
+    int [] array;
+    int n;
+
     @BeforeEach
-    void setUp() throws Exception {}
+    void setUp() throws Exception {
+        Random rand = new Random();
+        n = rand.nextInt(50);
+        n+=10;
+        array = new int[n];
+        for(int i =0;i<n;i++){
+            array[i] = i;
+        }
+    }
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void ElementIsInList() {
+        int [] array = new int[1];
+        array[0] = 1;
+        SearchResult result = BinarySearch.search(1,array);
+        assertEquals(true,result.isFound());
+        assertEquals(0,result.getPosition());
+    }
+
+    @Test
+    void ElementIsNotInList() {
+        int [] array = new int[1];
+        array[0] = 2;
+        SearchResult result = BinarySearch.search(1,array);
+        assertEquals(false,result.isFound());
+        assertEquals(-1,result.getPosition());
+    }
+
+    @Test
+    void ElementIsFirstElementInList() {
+        SearchResult result = BinarySearch.search(0,array);
+        assertEquals(true,result.isFound());
+        assertEquals(0,result.getPosition());
+    }
+
+    @Test
+    void ElementIsLastElementInList() {
+        SearchResult result = BinarySearch.search(n-1,array);
+        assertEquals(true,result.isFound());
+        assertEquals(n-1,result.getPosition());
+    }
+
+    @Test
+    void ElementIsMiddleElementInList() {
+        SearchResult result = BinarySearch.search(n/2,array);
+        assertEquals(true,result.isFound());
+        assertEquals(n/2,result.getPosition());
+    }
+
+    @Test
+    void ElementIsNotInListWithMultipleElements() {
+        SearchResult result = BinarySearch.search(n,array);
+        assertEquals(false,result.isFound());
+        assertEquals(-1,result.getPosition());
     }
 
 }
