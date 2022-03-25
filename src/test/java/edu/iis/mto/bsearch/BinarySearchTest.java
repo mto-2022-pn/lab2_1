637d28f6edca5eda661f7f2b462f98e87787bdb4
MateuszMatusiak <mateusz.matusiak000@gmail.com>
2022-03-25 08:37:43 +0100

    testy_v1 oneElementInSeq

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -12,8 +12,14 @@ class BinarySearchTest {
     void setUp() throws Exception {}
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void oneElementInSeq(){
+        int element = 20;
+        int[] seq = new int[1];
+        seq[0]=element;
+
+        SearchResult searchResult = BinarySearch.search(element,seq);
+        assertTrue(searchResult.isFound());
+        assertEquals(1, searchResult.getPosition());
     }
 
 }
