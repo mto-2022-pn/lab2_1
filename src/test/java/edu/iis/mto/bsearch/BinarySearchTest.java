2f0880412d0ada8ae1b70b3050092a12ab39898e
JakZaj <jakubzajac13@gmail.com>
2022-03-26 17:23:07 +0100

    testy_v1 Added .idea to .gitignore and unit test

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -9,11 +9,55 @@ import org.junit.jupiter.api.Test;
 class BinarySearchTest {
 
     @BeforeEach
-    void setUp() throws Exception {}
+    void setUp() throws Exception {
+    }
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void testSingleElementExistingInList() {
+        int[] seq = {1};
+        SearchResult searchResult = BinarySearch.search(seq[0], seq);
+        assertTrue(searchResult.isFound());
+        assertEquals(0, searchResult.getPosition());
+    }
+
+    @Test
+    void testSingleElementNotExistingInList() {
+        int[] seq = {1};
+        SearchResult searchResult = BinarySearch.search(2, seq);
+        assertFalse(searchResult.isFound());
+        assertEquals(-1, searchResult.getPosition());
+    }
+
+    @Test
+    void testMultipleElementFirst() {
+        int[] seq = {1, 2, 3, 4, 5};
+        SearchResult searchResult = BinarySearch.search(seq[0], seq);
+        assertTrue(searchResult.isFound());
+        assertEquals(0 ,searchResult.getPosition());
+    }
+
+    @Test
+    void testMultipleElementLast() {
+        int[] seq = {1, 2, 3, 4, 5};
+        SearchResult searchResult = BinarySearch.search(seq[seq.length - 1], seq);
+        assertTrue(searchResult.isFound());
+        assertEquals(seq.length - 1, searchResult.getPosition());
+    }
+
+    @Test
+    void testMultipleElementMid() {
+        int[] seq = {1, 2, 3, 4, 5};
+        SearchResult searchResult = BinarySearch.search(seq[(seq.length - 1) / 2], seq);
+        assertTrue(searchResult.isFound());
+        assertEquals((seq.length - 1) / 2,searchResult.getPosition());
+    }
+
+    @Test
+    void testMultipleElementNotExistingInList() {
+        int[] seq = {1, 2, 3, 4, 5};
+        SearchResult searchResult = BinarySearch.search(6, seq);
+        assertFalse(searchResult.isFound());
+        assertEquals(-1, searchResult.getPosition());
     }
 
 }
