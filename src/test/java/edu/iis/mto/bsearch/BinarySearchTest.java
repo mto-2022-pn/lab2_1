94295281d753d7a01137aa700e34ab4f6560b841
Karol Profic <karolprofic@protonmail.com>
2022-04-04 03:06:27 +0200

    testy_v1 Wstępne testy zgodnie z tabelą

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -12,8 +12,51 @@ class BinarySearchTest {
     void setUp() throws Exception {}
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void TestElementIsFoundInSequence() {
+        int[] seq = { 10 };
+        int key = 10;
+        SearchResult result = BinarySearch.search(key, seq);
+        assertTrue(result.isFound() && seq[result.getPosition()] == key);
+    }
+
+    @Test
+    void TestElementIsNotFoundInSequence() {
+        int[] seq = { 10 };
+        int key = 1;
+        SearchResult result = BinarySearch.search(key, seq);
+        assertFalse(result.isFound() && result.getPosition() == -1);
+    }
+
+    @Test
+    void TestElementIsFoundInSequenceAtTheBeginning() {
+        int[] seq = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int key = 1;
+        SearchResult result = BinarySearch.search(key, seq);
+        assertTrue(result.isFound() && seq[result.getPosition()] == key);
+    }
+
+    @Test
+    void TestElementIsFoundInSequenceInTheMiddle() {
+        int[] seq = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int key = 5;
+        SearchResult result = BinarySearch.search(key, seq);
+        assertTrue(result.isFound() && seq[result.getPosition()] == key);
+    }
+
+    @Test
+    void TestElementIsFoundInSequenceAtTheEnd() {
+        int[] seq = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int key = 9;
+        SearchResult result = BinarySearch.search(key, seq);
+        assertTrue(result.isFound() && seq[result.getPosition()] == key);
+    }
+
+    @Test
+    void TestElementIsFoundInSequenceWithManyElements() {
+        int[] seq = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int key = 10;
+        SearchResult result = BinarySearch.search(key, seq);
+        assertFalse(result.isFound() && result.getPosition() == -1);
     }
 
 }
