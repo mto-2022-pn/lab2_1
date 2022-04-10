a3548499c12a82670e1d5dcc5439bd2eace22dee
Oskar Schilling <80629512+GROMoOS@users.noreply.github.com>
2022-04-10 19:17:53 +0200

    testy_v2 fix BinarySearch to throw IllegalArgumentException

--- src/main/java/edu/iis/mto/bsearch/BinarySearch.java
+++ src/main/java/edu/iis/mto/bsearch/BinarySearch.java
@@ -23,6 +23,9 @@ public class BinarySearch {
      *         sekwencji, jezeli nie znaleziony -1)
      */
     public static SearchResult search(int key, int[] seq) {
+        if (seq == null || seq.length < 1) {
+            throw new IllegalArgumentException("Sequence is empty or not passed");
+        }
         int start = 0;
         int end = seq.length - 1;
         int center;
